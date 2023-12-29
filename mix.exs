92d3a11c3b3a6e546b3b669496d308e503e69604
Jake Morrison <jake@cogini.com>
2023-12-28 20:39:11 -0600

    Add extra_applications per env

--- mix.exs
+++ mix.exs
@@ -16,9 +16,14 @@ defmodule PhoenixContainerExample.MixProject do
   def application do
     [
       mod: {PhoenixContainerExample.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools] ++
+        extra_applications(Mix.env())
     ]
   end
+
+  defp extra_applications(:dev), do: [:tools]
+  defp extra_applications(:test), do: [:tools]
+  defp extra_applications(_), do: []
 
   # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
