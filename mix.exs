af81e7aeabc984822bd7e48fb179944f596cb53c
Jake Morrison <jake@cogini.com>
2023-12-28 20:43:35 -0600

    Add quality tools

--- mix.exs
+++ mix.exs
@@ -9,6 +9,28 @@ defmodule PhoenixContainerExample.MixProject do
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
+      dialyzer: [
+        # plt_add_deps: :project,
+        plt_add_apps: [:mix, :ex_unit]
+        # plt_add_deps: true,
+        # flags: ["-Werror_handling", "-Wrace_conditions"],
+        # flags: ["-Wunmatched_returns", :error_handling, :race_conditions, :underspecs],
+        # ignore_warnings: "dialyzer.ignore-warnings"
+      ],
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test,
+        "coveralls.lcov": :test,
+        quality: :test,
+        "quality.ci": :test,
+        "assets.deploy": :prod,
+        deploy: :prod
+      ],
+      default_release: :prod,
+      releases: releases(),
       deps: deps()
     ]
   end
@@ -31,13 +53,18 @@ defmodule PhoenixContainerExample.MixProject do
 
   defp deps do
     [
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.2", only: [:dev, :test], runtime: false},
       {:dns_cluster, "~> 0.1.1"},
       {:ecto_sql, "~> 3.10"},
       {:esbuild, "~> 0.8", runtime: Mix.env() == :dev},
+      {:excoveralls, "~> 0.18.0", only: [:dev, :test], runtime: false},
       {:finch, "~> 0.13"},
       {:floki, ">= 0.30.0", only: :test},
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
+      {:junit_formatter, "~> 3.3", only: [:dev, :test], runtime: false},
+      {:mix_audit, "~> 2.0", only: [:dev, :test], runtime: false},
       {:phoenix, "~> 1.7.10"},
       {:phoenix_ecto, "~> 4.4"},
       {:phoenix_html, "~> 3.3"},
@@ -46,6 +73,8 @@ defmodule PhoenixContainerExample.MixProject do
       {:phoenix_live_view, "~> 0.20.1"},
       {:plug_cowboy, "~> 2.5"},
       {:postgrex, ">= 0.0.0"},
+      {:sobelow, "~> 0.13", only: [:dev, :test], runtime: false},
+      {:styler, "~> 0.10.0", only: [:dev, :test], runtime: false},
       {:swoosh, "~> 1.3"},
       {:tailwind, "~> 0.2.0", runtime: Mix.env() == :dev},
       {:telemetry_metrics, "~> 0.6"},
@@ -61,7 +90,31 @@ defmodule PhoenixContainerExample.MixProject do
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
       "assets.setup": ["tailwind.install --if-missing", "esbuild.install --if-missing"],
       "assets.build": ["tailwind default", "esbuild default"],
-      "assets.deploy": ["tailwind default --minify", "esbuild default --minify", "phx.digest"]
+      "assets.deploy": ["tailwind default --minify", "esbuild default --minify", "phx.digest"],
+      quality: [
+        "test",
+        "format --check-formatted",
+        "credo",
+        # "credo --mute-exit-status",
+        # mix deps.clean --unlock --unused
+        "deps.unlock --check-unused",
+        # mix deps.update
+        # "hex.outdated",
+        # "hex.audit",
+        "deps.audit",
+        "sobelow --exit --quiet --skip -i DOS.StringToAtom,Config.HTTPS",
+        "dialyzer --quiet-with-result"
+      ],
+      "quality.ci": [
+        "format --check-formatted",
+        "deps.unlock --check-unused",
+        # "hex.outdated",
+        "hex.audit",
+        "deps.audit",
+        "credo",
+        "sobelow --exit --quiet --skip -i DOS.StringToAtom,Config.HTTPS",
+        "dialyzer --quiet-with-result"
+      ]
     ]
   end
 end
