a6cdc1e42f6584337f4e9656ff7b93d358e97f94
Jake Morrison <jake@cogini.com>
2023-12-29 22:18:42 -0600

    Configure junit formatter

--- config/test.exs
+++ config/test.exs
@@ -45,3 +45,11 @@ end
 
 # Initialize plugs at runtime for faster test compilation
 config :phoenix, :plug_init_mode, :runtime
+
+config :junit_formatter,
+  report_dir: "#{Mix.Project.build_path()}/junit-reports",
+  automatic_create_dir?: true,
+  # print_report_file: true,
+  # prepend_project_name?: true,
+  include_filename?: true,
+  include_file_line?: true
