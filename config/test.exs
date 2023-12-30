95e8a4f3bf78ce8f4ec7f64001a85a29c02b68e5
Jake Morrison <jake@cogini.com>
2023-12-29 22:18:59 -0600

    Delete boilerplate comments

--- config/test.exs
+++ config/test.exs
@@ -1,10 +1,5 @@
 import Config
 
-# Configure your database
-#
-# The MIX_TEST_PARTITION environment variable can be used
-# to provide built-in test partitioning in CI environment.
-# Run `mix help test` for more information.
 config :phoenix_container_example, PhoenixContainerExample.Repo,
   username: System.get_env("DATABASE_USER") || "postgres",
   password: System.get_env("DATABASE_PASS") || "postgres",
