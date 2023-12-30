d10e5d248595eb1f98be1676b8d956e1427e33b0
Jake Morrison <jake@cogini.com>
2023-12-29 22:19:23 -0600

    Delete boilerplate comments

--- lib/phoenix_container_example/application.ex
+++ lib/phoenix_container_example/application.ex
@@ -1,6 +1,4 @@
 defmodule PhoenixContainerExample.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
