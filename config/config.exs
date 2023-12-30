364c9efe940e9a9bb12915ca0bde820df476d273
Jake Morrison <jake@cogini.com>
2023-12-29 22:13:28 -0600

    Delete boilerplate comments

--- config/config.exs
+++ config/config.exs
@@ -1,17 +1,9 @@
-# This file is responsible for configuring your application
-# and its dependencies with the aid of the Config module.
-#
-# This configuration file is loaded before any dependency and
-# is restricted to this project.
-
-# General application configuration
 import Config
 
 config :phoenix_container_example,
   ecto_repos: [PhoenixContainerExample.Repo],
   generators: [timestamp_type: :utc_datetime]
 
-# Configures the endpoint
 config :phoenix_container_example, PhoenixContainerExampleWeb.Endpoint,
   url: [host: "localhost"],
   adapter: Phoenix.Endpoint.Cowboy2Adapter,
@@ -63,6 +55,4 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
-# Import environment specific config. This must remain at the bottom
-# of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
