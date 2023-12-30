8b24217bfc097407a19415226263d2f16bc6a964
Jake Morrison <jake@cogini.com>
2023-12-29 22:17:57 -0600

    Configure to use environment

--- config/test.exs
+++ config/test.exs
@@ -6,12 +6,12 @@ import Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :phoenix_container_example, PhoenixContainerExample.Repo,
-  username: "postgres",
-  password: "postgres",
-  hostname: "localhost",
-  database: "phoenix_container_example_test#{System.get_env("MIX_TEST_PARTITION")}",
+  username: System.get_env("DATABASE_USER") || "postgres",
+  password: System.get_env("DATABASE_PASS") || "postgres",
+  hostname: System.get_env("DATABASE_HOST") || "localhost",
+  database: System.get_env("DATABASE_DB") || "phoenix_container_example_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
-  pool_size: 10
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
