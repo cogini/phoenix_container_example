e6cf63faabf07c0f60bafe53c00d86b82f9ccfb5
Jake Morrison <jake@cogini.com>
2024-05-02 10:46:53 -0500

    Put in consistent order

--- config/test.exs
+++ config/test.exs
@@ -5,8 +5,8 @@ config :phoenix_container_example, PhoenixContainerExample.Repo,
   password: System.get_env("DATABASE_PASS") || "postgres",
   hostname: System.get_env("DATABASE_HOST") || "localhost",
   database: System.get_env("DATABASE_DB") || "app_test#{System.get_env("MIX_TEST_PARTITION")}",
-  pool: Ecto.Adapters.SQL.Sandbox,
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
