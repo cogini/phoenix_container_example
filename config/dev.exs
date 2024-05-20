970a6a854606a31fa5ea5ac3b78417c7c22e64b5
Jake Morrison <jake@cogini.com>
2024-05-20 10:42:44 -0500

    Use System.get_env default

--- config/dev.exs
+++ config/dev.exs
@@ -20,7 +20,7 @@ config :phoenix_container_example, PhoenixContainerExampleWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
   # http: [ip: {127, 0, 0, 1}, port: 4000],
-  http: [port: String.to_integer(System.get_env("PORT") || "4000")],
+  http: [port: String.to_integer(System.get_env("PORT", "4000"))],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
