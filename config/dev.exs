44f0d28f8cc8cc86513d3700efb628f2b985cb84
Jake Morrison <jake@cogini.com>
2024-07-21 14:09:47 -0500

    Use standard System.get_env

--- config/dev.exs
+++ config/dev.exs
@@ -7,13 +7,13 @@ config :phoenix_container_example, PhoenixContainerExample.Repo,
   database: System.get_env("DATABASE_DB") || "app_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
-  pool_size: String.to_integer(System.get_env("POOL_SIZE", "10"))
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
 config :phoenix_container_example, PhoenixContainerExampleWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
   # http: [ip: {127, 0, 0, 1}, port: 4000],
-  http: [port: String.to_integer(System.get_env("PORT", "4000"))],
+  http: [port: String.to_integer(System.get_env("PORT") || "4000")],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
