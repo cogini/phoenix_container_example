3aac3747dc5707c6dbdc8a89010283b602b63446
Jake Morrison <jake@cogini.com>
2025-04-22 17:56:07 -0500

    Alphabetize

--- config/test.exs
+++ config/test.exs
@@ -15,8 +15,13 @@ config :phoenix_container_example, PhoenixContainerExampleWeb.Endpoint,
   secret_key_base: "J6m9FJr/W+/oYmYQsRDIKWS3ZpCZGN9Xd/JP6qMvMzViO866jkg0lAZB/4yZjkCT",
   server: false
 
-# In test we don't send emails.
-config :phoenix_container_example, PhoenixContainerExample.Mailer, adapter: Swoosh.Adapters.Test
+config :junit_formatter,
+  report_dir: "#{Mix.Project.build_path()}/junit-reports",
+  automatic_create_dir?: true,
+  # print_report_file: true,
+  # prepend_project_name?: true,
+  include_filename?: true,
+  include_file_line?: true
 
 config :logger,
   level: :warning,
@@ -35,16 +40,11 @@ else
   config :opentelemetry, traces_exporter: :none
 end
 
+# In test we don't send emails.
+config :phoenix_container_example, PhoenixContainerExample.Mailer, adapter: Swoosh.Adapters.Test
+
 # Initialize plugs at runtime for faster test compilation
 config :phoenix, :plug_init_mode, :runtime
 
 # Disable swoosh api client as it is only required for production adapters.
 config :swoosh, :api_client, false
-
-config :junit_formatter,
-  report_dir: "#{Mix.Project.build_path()}/junit-reports",
-  automatic_create_dir?: true,
-  # print_report_file: true,
-  # prepend_project_name?: true,
-  include_filename?: true,
-  include_file_line?: true
