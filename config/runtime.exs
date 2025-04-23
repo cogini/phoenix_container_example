a8d491d350dac698b3ebe2807ef6d51d33a1b9cc
Jake Morrison <jake@cogini.com>
2025-04-23 13:11:15 -0500

    Configure ssh in dev

--- config/runtime.exs
+++ config/runtime.exs
@@ -33,6 +33,48 @@ config :ex_aws,
   secret_access_key: [{:system, "AWS_SECRET_ACCESS_KEY"}, :instance_role],
   region: System.get_env("AWS_REGION") || "us-east-1"
 
+if config_env() == :dev do
+  # host = System.get_env("PHX_HOST") || "example.com"
+  port = String.to_integer(System.get_env("PORT") || "4000")
+
+  secret_key_base =
+    System.get_env("SECRET_KEY_BASE") ||
+      raise """
+      environment variable SECRET_KEY_BASE is missing.
+      You can generate one by calling: mix phx.gen.secret
+      """
+
+  config :phoenix_container_example, PhoenixContainerExampleWeb.Endpoint,
+    # url: [host: host, port: 443, scheme: "https"],
+    # static_url: [host: "assets." <> host, port: 443, scheme: "https"],
+    http: [
+      # Enable IPv6 and bind on all interfaces.
+      # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
+      # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
+      # for details about using IPv6 vs IPv4 and loopback vs public addresses.
+      # ip: {0, 0, 0, 0, 0, 0, 0, 0},
+      ip: {0, 0, 0, 0},
+      port: port
+    ],
+    https:
+      Endpoint.https_opts(
+        System.get_env(),
+        %{
+          "HTTPS_CACERTS" => :cacerts,
+          "HTTPS_CACERTFILE" => :cacertfile,
+          "HTTPS_CERT" => :cert,
+          "HTTPS_CERTFILE" => :certfile,
+          "HTTPS_CIPHER_SUITE" => :cipher_suite,
+          "HTTPS_KEY" => :key,
+          "HTTPS_KEYFILE" => :keyfile,
+          "HTTPS_PORT" => :port
+        },
+        cipher_suite: :strong,
+        log_level: :warning
+      ),
+    secret_key_base: secret_key_base
+end
+
 if config_env() == :prod do
   database_url =
     System.get_env("DATABASE_URL") ||
