ed7b843b3db7e76ec85a44984e55456e83236a8e
Jake Morrison <jake@cogini.com>
2025-07-17 12:29:37 -0500

    Sync with current phx.new

--- lib/phoenix_container_example_web/telemetry.ex
+++ lib/phoenix_container_example_web/telemetry.ex
@@ -46,6 +46,7 @@ defmodule PhoenixContainerExampleWeb.Telemetry do
       summary("phoenix.socket_connected.duration",
         unit: {:native, :millisecond}
       ),
+      sum("phoenix.socket_drain.count"),
       summary("phoenix.channel_joined.duration",
         unit: {:native, :millisecond}
       ),
