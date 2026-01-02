18e3c42ca6d5f8ab30198fbeebb4c80bf5350791
Jake Morrison <jake@cogini.com>
2026-01-02 16:06:40 -0600

    Put in standard order

--- terraform/modules/iam-github-action/variables.tf
+++ terraform/modules/iam-github-action/variables.tf
@@ -1,10 +1,63 @@
+variable "aws_account_id" {
+  description = "AWS account id"
+  default     = ""
+}
+
+variable "codebuild_project_name" {
+  description = "Name of CodeBuild project"
+  default     = ""
+}
+
+# variable "codedeploy_name" {
+#   description = "Common name for CodeDeploy components"
+#   default = ""
+# }
+
+# variable "codedeploy_deploymentgroup_name" {
+#   description = "CodeDeploy deployment group name"
+#   default = ""
+# }
+
+# variable "codedeploy_deploymentconfig_name" {
+#   description = "CodeDeploy deployment config name"
+#   default = ""
+# }
+
+# variable "codedeploy_application_name" {
+#   description = "CodeDeploy application name"
+#   default = ""
+# }
+
+# variable "codedeploy_bucket" {
+#   description = "CodeDeploy bucket"
+#   default = ""
+# }
+
 variable "comp" {
   description = "Component"
 }
 
-variable "aws_account_id" {
-  description = "AWS account id"
-  default     = ""
+variable "ecr_arns" {
+  description = "ARNs of ECR repository"
+  type        = set(string)
+  default     = []
+}
+
+variable "ec2" {
+  description = "EC2 config"
+  type        = list(map(string))
+  default     = []
+}
+
+variable "ecs" {
+  description = "ECS config"
+  type        = list(map(string))
+  default     = []
+}
+
+variable "enable_cloudfront" {
+  description = "Enable CloudFront invalidation"
+  default     = false
 }
 
 variable "kms_default_key" {
@@ -46,60 +99,7 @@ variable "s3_buckets" {
   default     = []
 }
 
-variable "enable_cloudfront" {
-  description = "Enable CloudFront invalidation"
-  default     = false
-}
-
-variable "ecr_arns" {
-  description = "ARNs of ECR repository"
-  type        = set(string)
-  default     = []
-}
-
-variable "ec2" {
-  description = "EC2 config"
-  type        = list(map(string))
-  default     = []
-}
-
-variable "ecs" {
-  description = "ECS config"
-  type        = list(map(string))
-  default     = []
-}
-
-variable "codebuild_project_name" {
-  description = "Name of CodeBuild project"
-  default     = ""
-}
-
 # variable "enable_codedeploy" {
 #   description = "Enable CodeDeploy"
 #   default = false
-# }
-
-# variable "codedeploy_name" {
-#   description = "Common name for CodeDeploy components"
-#   default = ""
-# }
-
-# variable "codedeploy_deploymentgroup_name" {
-#   description = "CodeDeploy deployment group name"
-#   default = ""
-# }
-
-# variable "codedeploy_deploymentconfig_name" {
-#   description = "CodeDeploy deployment config name"
-#   default = ""
-# }
-
-# variable "codedeploy_application_name" {
-#   description = "CodeDeploy application name"
-#   default = ""
-# }
-
-# variable "codedeploy_bucket" {
-#   description = "CodeDeploy bucket"
-#   default = ""
 # }
