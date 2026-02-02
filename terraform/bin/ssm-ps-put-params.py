ef2215928c3f77dda352c1f068f635513faa8412
Jake Morrison <jake@cogini.com>
2026-02-01 19:28:39 -0600

    Doc

--- terraform/bin/ssm-ps-put-params.py
+++ terraform/bin/ssm-ps-put-params.py
@@ -5,8 +5,8 @@
 # CSV format:
 #   data_type: 'String' or 'SecureString'
 #   name: SSM key
-#   value: value
 #   env: OS environment var name
+#   value: value
 
 # Example:
 #   SecureString,db/url,DATABASE_URL,ecto://postgres:postgres@app-db.foo.internal/phoenix_container_example_dev
@@ -14,17 +14,25 @@
 #   SecureString,endpoint/secret_key_base,SECRET_KEY_BASE,lbMvIWnyI+ZMpA+FcN4HW2iUozEYgaOQcuQZ4yBYevI9QiOBFp0Jtj5FT96Rq3+T
 #   String,endpoint/host,PHX_HOST,example.com
 
-# SECRET_KEY_BASE = mix phx.gen.secret
-# RELEASE_COOKIE = Base.url_encode64(:crypto.strong_rand_bytes(40))
+# SECRET_KEY_BASE: mix phx.gen.secret
+# RELEASE_COOKIE: Base.url_encode64(:crypto.strong_rand_bytes(40))
+# LIVE_VIEW_SIGNING_SALT: mix phx.gen.secret 32
+# MQ_PASSWORD: mix phx.gen.secret 32
+# AUTH_KEY:
+#   ssh-keygen -N '' -t rsa -b 4096 -m PEM -f jwtRS256.key
+#   openssl rsa -in jwtRS256.key -pubout -outform PEM -out jwtRS256.key.pub
 
-# aws ssm get-parameters-by-path --path /cogini/foo/dev/app --no-paginate --region $AWS_REGION --query "Parameters[].{name:Name,valueFrom:Name}" --output json
+# Self-signed certificate for Phoenix, HTTPS_CERT and HTTPS_KEY:
+#   mix phx.gen.cert
+
+# aws ssm get-parameters-by-path --path /cogini/foo/dev/app --recursive --with-decryption --no-paginate --region $AWS_REGION --query "Parameters[].{name:Name,valueFrom:Name}" --output json
 
 import csv
 
 import argparse
 
 parser = argparse.ArgumentParser(description='Generate commands to set values in AWS SSM Parameter Store from a CSV file')
-parser.add_argument('-p', '--prefix', dest='prefix', help='SSM parameter name prefix, e.g. /cogini/foo/dev/app/', required=True)
+parser.add_argument('-p', '--prefix', dest='prefix', help='SSM parameter name prefix, e.g., /cogini/foo/dev/app/', required=True)
 parser.add_argument('-i', '--infile', dest='infile', help='Input CSV file', default='params.csv')
 
 args = parser.parse_args()
