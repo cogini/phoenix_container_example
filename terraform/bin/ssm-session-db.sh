1678c5d31bd038d78572e06de23291c0ba870a90
Jake Morrison <jake@cogini.com>
2026-02-01 19:28:56 -0600

    Use env vars

--- terraform/bin/ssm-session-db.sh
+++ terraform/bin/ssm-session-db.sh
@@ -8,12 +8,7 @@
 
 # curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/mac_arm64/session-manager-plugin.pkg" -o "session-manager-plugin.pkg"
 
-echo aws ssm start-session --region $AWS_REGION \
+echo aws ssm start-session --region "$AWS_REGION" \
     --target "$DEVOPS_INSTANCE" \
     --document-name AWS-StartPortForwardingSessionToRemoteHost \
-    --parameters host="$RDS_HOST",portNumber="5432",localPortNumber="25432"
-
-aws ssm start-session --region $AWS_REGION \
-    --target "$DEVOPS_INSTANCE" \
-    --document-name AWS-StartPortForwardingSessionToRemoteHost \
-    --parameters host="$RDS_HOST",portNumber="5432",localPortNumber="25432"
+    --parameters host="${RDS_HOST},portNumber=${RDS_PORT:-5432},localPortNumber=${RDS_LOCAL_PORT:-25432}"
