295e33eadc69ee13933f0a435a78020a9355b4ea
Jake Morrison <jake@cogini.com>
2026-04-14 19:56:50 -0500

    Vars

--- terraform/modules/target-group-default/variables.tf
+++ terraform/modules/target-group-default/variables.tf
@@ -3,48 +3,67 @@ variable "comp" {
   default     = "default"
 }
 
-# target group
+variable "deregistration_delay" {
+  description = "Draining time in secodns"
+  type        = number
+  default     = null
+}
+
+variable "health_check" {
+  description = "Mapping of tags for target group health_check"
+  type        = object({
+    enabled             = optional(bool)
+    healthy_threshold   = optional(number)
+    interval            = optional(number)
+    matcher             = optional(string)
+    path                = optional(string)
+    port                = optional(number)
+    protocol            = optional(string)
+    timeout             = optional(number)
+    unhealthy_threshold = optional(number)
+  })
+  default     = null
+}
+
 variable "name" {
   description = "The name of the target group"
   default     = ""
 }
 
 variable "port" {
-  description = "Listen port"
-  default     = "80"
+  description = "Port on which targets receive traffic"
+  type        = number
 }
 
 variable "protocol" {
-  description = "Target group protocol"
-  default     = "HTTP" # HTTP | HTTPS
-}
-
-variable "health_check" {
-  description = "Mapping of tags for target group health_check"
-  type        = map(any)
-  default     = {}
+  description = "Protocol to use for routing traffic to the targets"
+  type        = string
+  default     = null
 }
 
 variable "stickiness" {
-  description = "Mapping of tags for target group stickiness"
-  type        = map(any)
-  default = {
-    type            = "lb_cookie"
-    enabled         = false
-    cookie_duration = 86400
-  }
+  description = "Stickiness configuration"
+  type        = object({
+    cookie_duration = optional(number)
+    cookie_name     = optional(string)
+    enabled         = optional(bool)
+    type            = string
+  })
+  default     = null
 }
 
 variable "target_type" {
-  description = "The type of target, values are instance: instance, ip, lambda"
-  default     = "instance"
-}
-
-variable "vpc_id" {
-  description = "VPC id"
+  description = "The type of target, values are instance: instance, ip, lambda. Default instance"
+  type        = string
+  default     = null
 }
 
 variable "protocol_version" {
   description = "Protocol version"
-  default     = "HTTP1"
+  type        = string
+  default     = null
+}
+
+variable "vpc_id" {
+  description = "VPC id"
 }
