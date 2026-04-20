06ff9e8914221ba90e146fad47a156bc9600cb2f
Jake Morrison <jake@cogini.com>
2026-04-20 15:09:25 -0500

    Sort

--- terraform/modules/target-group/outputs.tf
+++ terraform/modules/target-group/outputs.tf
@@ -1,13 +1,3 @@
-output "name" {
-  description = "The name of the load balancer target group)"
-  value       = aws_lb_target_group.this.name
-}
-
-output "id" {
-  description = "The ARN of the load balancer target group (matches arn)"
-  value       = aws_lb_target_group.this.id
-}
-
 output "arn" {
   description = "The ARN of the default load balancer target group (matches id)"
   value       = aws_lb_target_group.this.arn
@@ -16,4 +6,14 @@ output "arn" {
 output "arn_suffix" {
   description = "The tg ARN suffix for use with CloudWatch Metrics"
   value       = aws_lb_target_group.this.arn_suffix
+}
+
+output "id" {
+  description = "The ARN of the load balancer target group (matches arn)"
+  value       = aws_lb_target_group.this.id
+}
+
+output "name" {
+  description = "The name of the load balancer target group)"
+  value       = aws_lb_target_group.this.name
 }
