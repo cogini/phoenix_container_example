88f4af969462403b9517dbd97ab56eeea08bc02d
Jake Morrison <jake@cogini.com>
2026-04-24 16:58:02 -0500

    Fix name

--- terraform/dev/us-east-1/dev/lambda-ecs-hook-app/src/index.py
+++ terraform/dev/us-east-1/dev/lambda-ecs-hook-app/src/index.py
@@ -7,7 +7,7 @@ import boto3
 from botocore.exceptions import ClientError
 
 from aws_lambda_powertools import Logger
-from aws_lambda_powertool.utilities.typing import LambdaContext
+from aws_lambda_powertools.utilities.typing import LambdaContext
 
 logger = Logger()
 
