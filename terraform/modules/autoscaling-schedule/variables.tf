c8031882a777ffe860ea31dbaaef93426d536085
Jake Morrison <jake@cogini.com>
2026-05-11 14:50:57 -0500

    Add autoscaling schedule

---
+++ terraform/modules/autoscaling-schedule/variables.tf
@@ -0,0 +1,10 @@
+# Scheduled Scaling
+variable "autoscaling_actions" {
+  description = "Define sheduled actions"
+  type        = map(any)
+  default     = {}
+}
+
+variable "comp" {
+  description = "Name of the app component, app, worker, etc."
+}
